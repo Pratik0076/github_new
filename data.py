d81e85db1d6d688342d4a355f2d2d338a87f29aa
Pratik Priyansu Patra <pratikpriyansu007@gmail.com>
2026-08-14 18:31:56 +0530

    Update data.py

--- data.py
+++ data.py
@@ -2,5 +2,5 @@ data = [
     {"id": 1, "name": "Pratik", "course": "BCA"},
     {"id": 2, "name": "Rahul", "course": "BCA"},
     {"id": 3, "name": "Aman", "course": "BCA"},
-    {"id": 4, "name": "Ram", "course": "BCA"}
+    {"id": 4, "name": "Ram", "course": "BA"}
 ]
