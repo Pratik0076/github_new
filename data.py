c1fcb2057eaaa81820dd9dff58adb5fec4c1d436
Pratik Priyansu Patra <pratikpriyansu007@gmail.com>
2026-08-14 18:12:29 +0530

    Add new student entry to data list

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 data = [
     {"id": 1, "name": "Pratik", "course": "BCA"},
     {"id": 2, "name": "Rahul", "course": "BCA"},
-    {"id": 3, "name": "Aman", "course": "BCA"}
+    {"id": 3, "name": "Aman", "course": "BCA"},
+    {"id": 4, "name": "Ram", "course": "BCA"}
 ]
